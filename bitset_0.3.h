3b747a989420b1841914bbd477c4a22bfa1964c4
drclcomputers <132164125+drclcomputers@users.noreply.github.com>
2024-03-24 13:20:05 +0200

    Update bitset_0.3.h

--- bitset_0.3.h
+++ bitset_0.3.h
@@ -1,26 +1,27 @@
+//Bitset Reimplementation
+//by DRCLComputers
 
-short int puteri[8] = { 1, 2, 4, 8, 16, 32, 64, 128 };
+short int puteri[8] = { 1, 2, 4, 8, 16, 32, 64, 128 }; //powers of 2
 
 class new_bitset{
 public:
-	unsigned int nr_grupa=0;
-	short int nr_byte=0;
-	unsigned char bitset[125000] = {};
+	unsigned int nr_grupa=0; // the number of the byte in the array
+	short int nr_byte=0; //the number of the bit in the byte
+	unsigned char bitset[125000] = {}; //the array
 
-	void input(int nr_adaugat) {
+	void input(int nr_adaugat) { //sets given bit to 1
 		nr_grupa = nr_adaugat / 8;
 		nr_byte = 7- nr_adaugat % 8;
-		bitset[nr_grupa] = bitset[nr_grupa] | puteri[nr_byte];
-		//std::cout << "nr din grupa" << int(bitset[nr_grupa]) << '\n';
+		bitset[nr_grupa] = bitset[nr_grupa] | puteri[nr_byte]; 
 	}
 
-	void reset(int nr_adaugat) {
+	void reset(int nr_adaugat) { //sets given bit to 0
 		nr_grupa = nr_adaugat / 8;
 		nr_byte = 7 - nr_adaugat % 8;
 		bitset[nr_grupa] = bitset[nr_grupa] & (~puteri[nr_byte]);
 	}
 
-	bool check(int nr_adaugat) {
+	bool check(int nr_adaugat) { //checks if given bit is true or false
 		nr_grupa = nr_adaugat / 8;
 		nr_byte = 7-nr_adaugat % 8;
 		int aux = bitset[nr_grupa];
@@ -40,15 +41,16 @@ public:
 			if (rest) return 1;
 			else return 0;
 		}
+		return 0;
 	}
 
-	void show(int start=1, int fin = 1000000) {
+	void show(int start=1, int fin = 1000000) { //prints the index of the 1 bits (optional, you can pass the first and last bit you desire) 
 		for (int i = start; i <= fin; i++) {
 			if (check(i)) std::cout << i << " ";
 		}
 	}
 
-	int count(int start = 1, int fin = 1000000) {
+	int count(int start = 1, int fin = 1000000) { //counts how many bits are set to true in the array (optional, you can pass the first and last bit you desire)
 		int aux = 0;
 		for (int i = start; i <= fin; i++) {
 			if (check(i)) aux++;
